f7d97bc5f2ebb70d16780a58ee6653be4b26e139
ira-kabalyuk <jokky@bigmir.net>
2016-12-03 22:15:49 +0200

    section-4

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -37,43 +37,17 @@ jQuery(document).ready(function() {
     $('.rightfly').addClass("is-hidden").viewportChecker({
         classToAdd: 'visible animated bounceInRight',
         offset: 100
-    });  
+    });
+
+    $('.dance').addClass("is-hidden").viewportChecker({
+        classToAdd: 'visible animated swing',
+        offset: 100
+    });   
 
     $(".input-head").click(function() {        
         $(".input-list").toggleClass("in-visible");
     });
-
     
-
-    $('#fullpage').fullpage({       
-
-        //Scrolling
-        css3: true,
-        scrollingSpeed: 700,
-        autoScrolling: true,
-        fitToSection: true,
-        fitToSectionDelay: 1000,
-        scrollBar: false,
-        easing: 'easeInOutCubic',
-        easingcss3: 'ease',
-        loopBottom: false,
-        loopTop: false,
-        loopHorizontal: true,
-        continuousVertical: false,
-        continuousHorizontal: false,
-        scrollHorizontally: false,
-        interlockedSlides: false,
-        dragAndMove: false,
-        offsetSections: false,
-        resetSliders: false,
-        fadingEffect: false,
-        normalScrollElements: '#element1, .element2',
-        scrollOverflow: false,
-        scrollOverflowOptions: null,
-        touchSensitivity: 15,
-        normalScrollElementTouchThreshold: 5,
-        bigSectionsDestination: null,        
-    });
 
 });
 
